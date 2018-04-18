2e1b06291037f183a32185d6169a5a8a9db54c33
MindFiend <Austinpapow@gmail.com>
2018-04-18 16:38:06 +0000

    use round function

--- chapter4-gpios/dht.js
+++ chapter4-gpios/dht.js
@@ -2,22 +2,27 @@ var sensorLib = require('node-dht-sensor');
 
 sensorLib.initialize(22, 12); //#A
 var interval = setInterval(function () { //#B
-  read();
+	  read();
 }, 2000);
 
 function read() {
-  var readout = sensorLib.read(); //#C
-  console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + //#D
-    'humidity: ' + readout.humidity.toFixed(2) + '%');
+	  var readout = sensorLib.read(); //#C
+	  var TempinC =  readout.temperature.toFixed(2);
+	  var TempinF = TempinC * 9 / 5 +32;
+	  var TempinF = Math.round(TempinF * 10) / 10;
+	  console.log('Temperature: ' + TempinF + 'F, ' + 
+		  'humidity: ' + readout.humidity.toFixed(2) + '%');
+//	  console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + //#D
+//		      'humidity: ' + readout.humidity.toFixed(2) + '%');
 };
 
 process.on('SIGINT', function () {
-  clearInterval(interval);
-  console.log('Bye, bye!');
-  process.exit();
+	  clearInterval(interval);
+	  console.log('Bye, bye!');
+	  process.exit();
 });
 
 //#A 22 is for DHT22/AM2302, 12 is the GPIO we connect to on the Pi
-//#B create an interval to read the values every 2 seconds
-//#C read the sensor values
-//#D readout contains two values: temperature and humidity
+////#B create an interval to read the values every 2 seconds
+////#C read the sensor values
+////#D readout contains two values: temperature and humidity
